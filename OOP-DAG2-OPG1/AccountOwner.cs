260369dc3c023d5f647fe72b9c56da49d1805925
andre <andre@ANDREAS_LAPTOP>
2024-11-05 13:08:03 +0100

    Add project files.

---
+++ OOP-DAG2-OPG1/AccountOwner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Bank
+{
+    internal class AccountOwner
+    {
+        public int ID { get; private set; }
+        public string FullName { get; private set; }
+
+        // Constructor with ID and name parameters
+        public AccountOwner(int id, string name)
+        {
+            ID = id;
+            FullName = name;
+        }
+
+        public Account Owner { get; set; }
+
+        public void UserInfo()
+        {
+            Console.WriteLine($"Konto ejer: {FullName}, ID: {ID}");
+        }
+    }
+}
